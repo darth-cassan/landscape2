754a258215ef47affffdbe76ca48ee9ef08eefe6
Daniel Castaño Sánchez <danielcastanosanchez@icloud.com>
2023-10-10 20:57:17 +0200

    Add some more checks
    Signed-off-by: Daniel Castaño Sánchez <danielcastanosanchez@icloud.com>

--- src/build/settings.rs
+++ src/build/settings.rs
@@ -89,14 +89,59 @@ impl LandscapeSettings {
         Ok(settings)
     }
 
-    /// Validate landscape settings
+    /// Validate landscape settings.
     fn validate(&self) -> Result<()> {
         // Check foundation is not empty
         if self.foundation.is_empty() {
             return Err(format_err!("foundation cannot be empty"));
         }
 
-        // Check colors format
+        // Check members category is not empty
+        if let Some(members_category) = &self.members_category {
+            if members_category.is_empty() {
+                return Err(format_err!("members category cannot be empty"));
+            }
+        }
+
+        self.validate_categories()?;
+        self.validate_colors()?;
+        self.validate_featured_items()?;
+        self.validate_groups()?;
+
+        Ok(())
+    }
+
+    /// Check categories are valid
+    fn validate_categories(&self) -> Result<()> {
+        if let Some(categories) = &self.categories {
+            for (i, category) in categories.iter().enumerate() {
+                let category_id = if category.name.is_empty() {
+                    format!("{i}")
+                } else {
+                    category.name.clone()
+                };
+
+                // Name
+                if category.name.is_empty() {
+                    return Err(format_err!("category [{category_id}] name cannot be empty"));
+                }
+
+                // Subcategories
+                for (i, subcategory) in category.subcategories.iter().enumerate() {
+                    if subcategory.is_empty() {
+                        return Err(format_err!(
+                            "category [{category_id}]: subcategory [{i}] cannot be empty"
+                        ));
+                    }
+                }
+            }
+        }
+
+        Ok(())
+    }
+
+    /// Check colors format
+    fn validate_colors(&self) -> Result<()> {
         if let Some(colors) = &self.colors {
             let colors = [
                 ("color1", &colors.color1),
@@ -106,6 +151,7 @@ impl LandscapeSettings {
                 ("color5", &colors.color5),
                 ("color6", &colors.color6),
             ];
+
             for (name, value) in colors {
                 if !RGBA.is_match(value) {
                     return Err(format_err!(
@@ -115,42 +161,69 @@ impl LandscapeSettings {
             }
         }
 
-        // Check groups are valid
-        if let Some(groups) = &self.groups {
-            for (group_index, group) in groups.iter().enumerate() {
-                let group_id = if group.name.is_empty() {
-                    format!("{group_index}")
+        Ok(())
+    }
+
+    /// Check featured item rules are valid
+    fn validate_featured_items(&self) -> Result<()> {
+        if let Some(featured_items) = &self.featured_items {
+            for (i, rule) in featured_items.iter().enumerate() {
+                let rule_id = if rule.field.is_empty() {
+                    format!("{i}")
                 } else {
-                    group.name.clone()
+                    rule.field.clone()
                 };
-                if group.name.is_empty() {
-                    return Err(format_err!("group [{group_id}] name cannot be empty"));
+                // Se duplica
+                let ctx = format!("rule [{}] is not valid (field: [{}])", rule_id, rule.field);
+
+                // Field
+                if rule.field.is_empty() {
+                    return Err(format_err!("field cannot be empty")).context(ctx);
                 }
-                for (category_index, category) in group.categories.iter().enumerate() {
-                    if category.is_empty() {
-                        return Err(format_err!(
-                            "group [{group_id}]: category [{category_index}] cannot be empty"
-                        ));
+
+                // Options
+                if rule.options.is_empty() {
+                    return Err(format_err!("options cannot be empty")).context(ctx);
+                }
+                for option in &rule.options {
+                    // Value
+                    if option.value.is_empty() {
+                        return Err(format_err!("option value cannot be empty")).context(ctx);
+                    }
+
+                    // Label
+                    if let Some(label) = &option.label {
+                        if label.is_empty() {
+                            return Err(format_err!("option label cannot be empty")).context(ctx);
+                        }
                     }
                 }
             }
         }
 
-        // Check categories are valid
-        if let Some(categories) = &self.categories {
-            for (category_index, category) in categories.iter().enumerate() {
-                let category_id = if category.name.is_empty() {
-                    format!("{category_index}")
+        Ok(())
+    }
+
+    /// Check groups are valid
+    fn validate_groups(&self) -> Result<()> {
+        if let Some(groups) = &self.groups {
+            for (i, group) in groups.iter().enumerate() {
+                let group_id = if group.name.is_empty() {
+                    format!("{i}")
                 } else {
-                    category.name.clone()
+                    group.name.clone()
                 };
-                if category.name.is_empty() {
-                    return Err(format_err!("category [{category_id}] name cannot be empty"));
+
+                // Name
+                if group.name.is_empty() {
+                    return Err(format_err!("group [{group_id}] name cannot be empty"));
                 }
-                for (subcategory_index, subcategory) in category.subcategories.iter().enumerate() {
-                    if subcategory.is_empty() {
+
+                // Categories
+                for (category_index, category) in group.categories.iter().enumerate() {
+                    if category.is_empty() {
                         return Err(format_err!(
-                            "category [{category_id}]: subcategory [{subcategory_index}] cannot be empty"
+                            "group [{group_id}]: category [{category_index}] cannot be empty"
                         ));
                     }
                 }
